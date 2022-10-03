f805586ba773718f97832fd92f8fad7b18a4ad01
vaishnavisriyeluri <113297084+vaishnavisriyeluri@users.noreply.github.com>
2022-10-04 00:02:58 +0530

    Add files via upload

---
+++ Main4.java
@@ -0,0 +1,11 @@
+class Main4 {
+  public static void main(String[] args) {
+    System.out.println((5 > 3) && (8 > 5));  
+    System.out.println((5 > 3) && (8 < 5));  
+    System.out.println((5 < 3) || (8 > 5));  
+    System.out.println((5 > 3) || (8 < 5));  
+    System.out.println((5 < 3) || (8 < 5));  
+    System.out.println(!(5 == 3));  
+    System.out.println(!(5 > 3));  
+  }
+}
